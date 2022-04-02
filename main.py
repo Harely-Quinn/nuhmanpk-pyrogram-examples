3d5333ef9c610caa9dbb71f0d6725cdb42e7d91d
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-02 21:09:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pyrogram
 from pyromod import listen
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 Bot = Client(
     "Instant-Caption-Adder",
@@ -76,6 +77,32 @@ async def ban(bot, message):
     else:
         return
 
+
+keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Example Inline Link Button",
+                        url=f"https://github.com/swatv3nub/PyrogramBot",
+                    ),
+                    InlineKeyboardButton(
+                        text="Example Inline Query Button",
+                        callback_data="example",
+                    )
+                ]
+
+#Start
+
+@app.on_message(filters.command("start"))
+@capture_err
+async def start(_, message):
+    start_text=f"Hello {message.from_user.mention}, I am {BOT_NAME}\n\nMy Onwer is [{OWNER_ID}](tg://user?id={OWNER_ID})"
+    await message.reply_text(start_text, reply_markup=keyboard, parse_mode="markdown")
+    
+
+@app.on_callback_query(filters.regex("example"))
+async def example(_, CallBackQuery):
+    await app.answer_callback_query(CallbackQuery.id, "Surprise!", show_alert=True)
 
 Bot.run()
 
