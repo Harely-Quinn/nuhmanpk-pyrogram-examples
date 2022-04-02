2451f646a5dbe047c39a6e0a2b3eb50e557e1a76
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-02 20:55:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -77,16 +77,6 @@ async def ban(bot, message):
     else:
         return
 
-@Bot.on_message(filters.new_chat_members)
-async def welcome(bot,message):
-	chatid= message.chat.id
-	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
-	
-@Bot.on_message(filters.left_chat_member)
-async def goodbye(bot,message):
-	chatid= message.chat.id
-	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
-	
 
 Bot.run()
 
