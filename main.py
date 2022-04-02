31d7ba402feac172afe7b32e5caba5506dbd549c
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-02 19:08:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ bot = Client(
     api_hash = os.environ["API_HASH"]
 )
 
-@bot.on_message(filters.edited)
-async def edited(bot,message):
-	chatid= message.chat.id	
-	await bot.send_message(text=f"{message.from_user.mention} Edited This [Message]({message.link})",chat_id=chatid)
-	
 
 @bot.on_message(filters.forwarded)
 async def forward(bot, message):
